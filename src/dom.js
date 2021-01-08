b13520c37eac7600fd981c532bda7c663b10e6a6
Gabriel Lima <gabrielpk1@live.com>
2021-01-07 23:18:50 -0300

    add main styles and some dom functions

--- src/dom.js
+++ src/dom.js
@@ -1,5 +1,12 @@
 const Dom = ()=> {
 
+    function createH1(id, text){
+        const h1 = document.createElement("h1");
+        h1.id = id;
+        h1.textContent = text;
+        return h1;
+    }
+
     function createImg(id, src){
         const img = document.createElement("img");
         img.id = id;
@@ -11,6 +18,12 @@ const Dom = ()=> {
         const div = document.createElement("div");
         div.id = id;
         return div;
+    }
+
+    function createSpan(classList){
+        const span = document.createElement("span");
+        classList.forEach(clas=> span.classList.add(clas));
+        return span;
     }
     
     function createLabel({id, text}){
@@ -64,6 +77,36 @@ const Dom = ()=> {
         appendNode(form, ...labels);
         return form;
     }
+
+    function createHeader() {
+        const header = document.createElement("header");
+        const h1 = createH1("title", "TODO LIST");
+        const menu = createDiv("menu");
+        const menuBtn = createDiv("menu-btn");
+        for(let i = 0; i < 3; i++){
+            let span = createSpan(["line"]);
+            appendNode(menuBtn, span);
+        }
+        const home = createDiv("home");
+        appendNode(menu, menuBtn, home);
+        appendNode(header, menu, h1);
+        return header;
+    }
+
+    function createNav(id){
+        const nav = document.createElement("nav");
+        nav.id = id;
+        return nav;
+    }
+
+    function createMain(){
+        const main = document.createElement("main");
+        const nav = createNav("side-menu");
+        const div = createDiv("container");
+        appendNode(main, nav, div);
+        return main;
+    }
+
     
     function renderLoginWindow(){
         const body = document.querySelector("body");
@@ -85,16 +128,22 @@ const Dom = ()=> {
         appendNode(form, div, demoBtn);
         appendNode(body, img, form);
     }
+
+    function renderMainContent(){
+        const body = document.querySelector("body");
+        const header = createHeader();
+        const main = createMain();
+        appendNode(body, header, main);
+    }
     
     function removeBodyContent() {
-        console.log("It's time");
         const body = document.querySelector("body");
         body.style.background = "none";
         const children = Array.from(body.children);
         children.forEach(child=> body.removeChild(child));
     }
 
-    return {renderLoginWindow, removeBodyContent}
+    return {renderLoginWindow, removeBodyContent, renderMainContent}
 }
 
 const dom = Dom();
--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import {demoEvent} from './events';
 // dom.renderLoginWindow();
 // const btn = document.querySelector(".demo");
 // btn.addEventListener("click", demoEvent);
+// dom.renderMainContent();
